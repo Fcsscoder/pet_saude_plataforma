90f44ee60fbd8696c69d00d38b8ae1194d7c72a2
Fcsscoder <caiotosousa43@gmail.com>
2025-10-31 13:07:31 -0300

    ajustando telas de auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import './App.css';
 
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 function App() {
+  const location = useLocation();
+  const hideLayout =
+    location.pathname === '/entrar' || location.pathname === '/cadastrar';
+  console.log(hideLayout);
   return (
     <>
       <div className="p-0">
         <Navbar />
         <Outlet />
-        <Footer />
+        {!hideLayout && <Footer />}
       </div>
     </>
   );
